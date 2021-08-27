7a7c65df63d7b223ecab5d093791a978f3852c1e
Leydson Vieira <leydson.vieira@gmail.com>
2021-08-26 23:42:58 -0300

    Change: modules structure

--- idealers/idealers/urls.py
+++ idealers/idealers/urls.py
@@ -1,4 +1,4 @@
-"""dealers URL Configuration
+"""idealers URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/

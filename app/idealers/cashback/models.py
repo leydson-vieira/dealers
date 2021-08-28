6442ecbf9c1475ca0f9fef4377adc82f459b4033
Leydson Vieira <leydson.vieira@gmail.com>
2021-08-27 22:51:58 -0300

    Add: Cashback data model

--- app/idealers/cashback/models.py
+++ app/idealers/cashback/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Cashback(models.Model):
+    """"
+    DB Model used to save Cashback records and its values
+    """
+    order = models.OneToOneField(
+        'orders.Order',
+        on_delete=models.DO_NOTHING,
+        primary_key=True
+    )
+
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    percentage = models.DecimalField(max_digits=3, decimal_places=2)
+
+    def __str__(self) -> str:
+        return f'Cashback for: {self.order}'

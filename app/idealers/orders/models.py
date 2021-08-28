a9280ece57615559440d9fc6ca2bb143864e254e
Leydson Vieira <leydson.vieira@gmail.com>
2021-08-27 23:35:31 -0300

    Adjust Order Model

--- app/idealers/orders/models.py
+++ app/idealers/orders/models.py
@@ -13,23 +13,26 @@ class Order(models.Model):
         REJECTED = 'tejected', 'Rejeitado'
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    code = models.CharField(max_length=150, )
+    code = models.CharField(max_length=150)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     status = models.CharField(
         max_length=13,
-        choices=Status,
+        choices=Status.choices,
         default=Status.IN_VALIDATION,
     )
 
     dealer = models.ForeignKey(
         'authentication.Dealer',
         on_delete=models.DO_NOTHING,
-        to_field='cpf',
         related_name='orders',
     )
 
     def __str__(self) -> str:
         return f'Order: {self.id}'
+
+    @property
+    def has_cashback(self):
+        return hasattr(self, 'cashback')
